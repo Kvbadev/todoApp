465cb04d169c9774f532c1b2c0d6fb22487b57ea
Jakub Piasecki <jakubpiasecki05@gmail.com>
2021-05-26 19:11:00 +0200

    zachowaj zamiast zalicz

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
+
+//BUTTONS
 const addButton = document.querySelector('.btn');
 const input = document.getElementById('textplace')
 const taskList = document.querySelector('.tasks');
 const page = document.querySelector('.page');
+const btnPage1 = document.getElementById('btn_page1');
+const btnPage2 = document.getElementById('btn_page2');
+//
+
+//VARIABLES
 var newTask = null;
 var number = 0;
+var elementId = "";
+//
 
+//EVENT LISTENERS
 addButton.addEventListener('click', function(){
     if(input.value != ""){
         newTask = document.createElement("div");
         newTask.textContent = input.value;
         taskList.appendChild(newTask);
-        newTask.classList.add('task');
-        newTask.id = "task" + number;
+        newTask.classList.toggle('task');
+        newTask.id = getId();
         input.value = "";
         number++;
         if(number > 0){
             newTask.addEventListener('click', function(){
-                page.classList.add('page2');
-                console.log(newTask.id)
+                page.classList.toggle('page2');
+                elementId = getAnId(this);   
             })
         }
     }
 })
+btnPage1.addEventListener('click', function(){
+    page.classList.toggle('page2');
+})
 
-if(number > 0){
-    newTask.addEventListener('click', function(){
-        // page.classList.add('page2');
-        console.log('ldfksl');
-    })
+btnPage2.addEventListener('click', function(){
+    document.getElementById(elementId).remove();
+    page.classList.toggle('page2');
+})
+
+
+function getId(){
+    let taskId = "task" + number;
+    return taskId;
 }
-
+function getAnId(btn){
+    return(btn.id);
+}
