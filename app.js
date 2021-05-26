fb8f3cea6813589617a4304e3e5a048b9db623a5
Jakub Piasecki <jakubpiasecki05@gmail.com>
2021-05-26 11:54:28 +0200

    updates

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const addButton = document.querySelector('.btn');
 const input = document.getElementById('textplace')
 const taskList = document.querySelector('.tasks');
+const page = document.querySelector('.page');
+var newTask = null;
+var number = 0;
 
 addButton.addEventListener('click', function(){
-    // if(inputArea.textContent != null){
-        var newTask = document.createElement("p");
+    if(input.value != ""){
+        newTask = document.createElement("div");
         newTask.textContent = input.value;
         taskList.appendChild(newTask);
         newTask.classList.add('task');
+        newTask.id = "task" + number;
         input.value = "";
-    // }
+        number++;
+        if(number > 0){
+            newTask.addEventListener('click', function(){
+                page.classList.add('page2');
+                console.log(newTask.id)
+            })
+        }
+    }
 })
+
+if(number > 0){
+    newTask.addEventListener('click', function(){
+        // page.classList.add('page2');
+        console.log('ldfksl');
+    })
+}
+
